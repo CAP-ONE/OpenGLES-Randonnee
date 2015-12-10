eebcf1f982120dfe5d1e6e1081e637ed09ad8f28
ROD <capone973@gmail.com>
2015-12-10 16:58:22 +0100

    ajout libs cardboard

--- app/build/generated/source/r/all/debug/com/android/androidGL4D/R.java
+++ app/build/generated/source/r/all/debug/com/android/androidGL4D/R.java
@@ -10,11 +10,16 @@ package com.android.androidGL4D;
 public final class R {
     public static final class attr {
     }
-    public static final class drawable {
-        public static final int ciel=0x7f020000;
+    public static final class id {
+        public static final int cardboard_view=0x7f060001;
+        public static final int overlay=0x7f060002;
+        public static final int ui_layout=0x7f060000;
+    }
+    public static final class layout {
+        public static final int common_ui=0x7f030000;
     }
     public static final class mipmap {
-        public static final int ic_launcher=0x7f030000;
+        public static final int ic_launcher=0x7f020000;
     }
     public static final class raw {
         public static final int basic_fs=0x7f040000;
